93a0c015e46d400c40b7da5e30bfdfc0182c6c81
Muktar Ali <muktarali1004@gmail.com>
2025-07-24 15:12:43 -0400

    Hover effect created through mouseevent

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const container = document.querySelector("#container");
-let content = document.createElement("div");
 
 for(let i =0 ;  i < 4; i++){
     for(let j= 0 ; j < 4;j++){
-    content = document.createElement("div");
+    const content = document.createElement("div");
     content.classList.add("content");
     container.appendChild(content);
+    content.addEventListener('mouseenter', () => {
+    content.style.backgroundColor = "lightblue";
+});
+  
     }
+     
 }
 
+ 
 
 
 
