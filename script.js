4822bc867795546dc118e61cc6e5ad1d53a45eca
Muktar Ali <muktarali1004@gmail.com>
2025-07-24 17:41:02 -0400

    Added dynamic creation of grid layout

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const container = document.querySelector("#container");
-
-for(let i =0 ;  i < 4; i++){
-    for(let j= 0 ; j < 4;j++){
-    const content = document.createElement("div");
-    content.classList.add("content");
-    container.appendChild(content);
-    content.addEventListener('mouseenter', () => {
-    content.style.backgroundColor = "lightblue";
-});
-  
+const squares = document.getElementById("my-button");
+squares.addEventListener('click' , () =>{
+    let numSquares = parseInt(prompt("How many squares do you want?"));
+    if(numSquares >= 100){
+        alert("Enter number less than 100");
+        return;
     }
-     
-}
+    let squareSize = (800 / numSquares);
+    for(let i = 0 ;  i < numSquares; i++){
+        for(let j= 0 ; j < numSquares;j++){
+        const content = document.createElement("div");
+        content.classList.add("content");
+        content.style.height = `${squareSize}px`;
+        content.style.width = `${squareSize}px`;
+        container.appendChild(content);
+        content.addEventListener('mouseenter', () => {
+        content.style.backgroundColor = "lightblue";
+            });
+        }  
+    }
+});
+
+
 
  
 
